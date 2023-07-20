02ec9882cbf7ad0ede45e02b787bc228eb872f47
Ayça Işık <117450558+aycaisik@users.noreply.github.com>
2023-07-20 15:37:44 +0300

    List and Identifiable Protocol added.

--- H4X0R News/ContentView.swift
+++ H4X0R News/ContentView.swift
@@ -9,18 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+                List(posts){ post in
+                    Text(post.title)
+                }
+            .navigationBarTitle("H4X0R NEWS")
+            }
         }
-        .padding()
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+struct Post : Identifiable {
+    let id : String
+    let title : String
+}
+let posts = [
+Post(id: "1", title: "hey"),
+Post(id: "2", title: "hello"),
+Post(id: "3", title: "bonjour")
+]
