042aeb30b555dd4c948c839d735d8549b0ef9482
Ayça Işık <117450558+aycaisik@users.noreply.github.com>
2023-07-20 15:06:06 +0300

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Ayça Işık on 20.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
